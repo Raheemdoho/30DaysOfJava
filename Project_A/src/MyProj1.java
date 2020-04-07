82dd187c4c9545b0606eb315331b3c0820def872
Akhigbe <OAkhigbe@Elections.ca>
2020-04-07 14:29:04 -0400

    add new files

---
+++ Project_A/src/MyProj1.java
@@ -0,0 +1,18 @@
+
+public class MyProj1 {
+
+		public static void main(String[] args) { //method
+		System.out.println("Hello there!");
+		
+	}
+}
+
+//comment
+//class - contains everything - has members
+//methods - do something
+//statement - telling the computer to do something
+//properties - store something
+
+//access modifies (public) - who can use?
+//static - No instance of class is needed
+//object - instance of a class
---
+++ Project_A/src/MyProj2.java
@@ -0,0 +1,20 @@
+
+public class MyProj2 {
+
+	public static void main(String[] args) { //method
+		System.out.println("Hello " + args[0]);
+		
+	}
+}
+
+//comment
+//class - contains everything - has members
+//methods - do something
+//arguments - what you pass to a method (part of the calling of the method)
+//parameter - variable to store arguments (part of definition of a method)
+//statement - telling the computer to do something
+//properties - store something
+
+//access modifies (public) - who can use?
+//static - No instance of class is needed
+//object - instance of a class

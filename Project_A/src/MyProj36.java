4958db29d52986664931029295f14a3bf48274fd
Okhaide <53632924+Raheemdoho@users.noreply.github.com>
2020-04-27 05:32:48 -0400

    earns_object

---
+++ Project_A/src/MyProj36.java
@@ -0,0 +1,13 @@
+
+public class MyProj36 {
+
+	public static void main(String[] args) {
+		User2 user = new User2();
+		user.firstName = "Sub";
+		user.lastName = "Scriber";
+		
+		user.output();
+		
+	}
+
+}
---
+++ Project_A/src/User2.java
@@ -0,0 +1,8 @@
+public class User2 {
+	public String firstName;
+	public String lastName;
+
+	public void output() {
+		System.out.println(firstName + " " + lastName);
+	}
+}
